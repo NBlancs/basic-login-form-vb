e12653b01cc55f79e7c68f72d3a492a3028abde8
noel <bnoel.omar@gmail.com>
2024-12-23 13:31:13 +0800

    Add project files.

---
+++ basic-login-form-vb/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        usernametxt = New TextBox()
+        loginbtn = New Button()
+        passwordtxt = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(301, 82)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(181, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "Basic Login Form"
+        ' 
+        ' usernametxt
+        ' 
+        usernametxt.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        usernametxt.Location = New Point(236, 150)
+        usernametxt.Name = "usernametxt"
+        usernametxt.PlaceholderText = "username"
+        usernametxt.Size = New Size(300, 35)
+        usernametxt.TabIndex = 1
+        ' 
+        ' loginbtn
+        ' 
+        loginbtn.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        loginbtn.Location = New Point(326, 299)
+        loginbtn.Name = "loginbtn"
+        loginbtn.Size = New Size(128, 50)
+        loginbtn.TabIndex = 3
+        loginbtn.Text = "Login"
+        loginbtn.UseVisualStyleBackColor = True
+        ' 
+        ' passwordtxt
+        ' 
+        passwordtxt.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        passwordtxt.Location = New Point(236, 224)
+        passwordtxt.Name = "passwordtxt"
+        passwordtxt.PasswordChar = "*"c
+        passwordtxt.PlaceholderText = "password"
+        passwordtxt.Size = New Size(300, 35)
+        passwordtxt.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(passwordtxt)
+        Controls.Add(loginbtn)
+        Controls.Add(usernametxt)
+        Controls.Add(Label1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents usernametxt As TextBox
+    Friend WithEvents loginbtn As Button
+    Friend WithEvents passwordtxt As TextBox
+
+End Class
---
+++ basic-login-form-vb/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialization code if needed
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles usernametxt.TextChanged
+        ' Handle text changed event if needed
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles loginbtn.Click
+        Dim username As String = usernametxt.Text
+        Dim password As String = passwordtxt.Text
+
+        ' Check if the username and password are empty
+        If String.IsNullOrWhiteSpace(username) Then
+            MessageBox.Show("Please input a username.")
+            Return
+        End If
+
+        If String.IsNullOrWhiteSpace(password) Then
+            MessageBox.Show("Please input a password.")
+            Return
+        End If
+
+        ' Login Logic
+        If username = "admin" AndAlso password = "password" Then
+            MessageBox.Show("Login successful!")
+        Else
+            MessageBox.Show("Invalid username or password.")
+        End If
+    End Sub
+
+    Private Sub passwordtxt_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs)
+        ' Handle mask input rejection if needed
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles passwordtxt.TextChanged
+
+    End Sub
+End Class
